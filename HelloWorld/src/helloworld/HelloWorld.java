d8b1a8d665a774bfc3b5bc07edad30d6d481d2cf
Stepan_Novikov <novikovstepan@gmail.com>
2017-12-26 13:34:23 +0200

    Code cleanup

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Simple Hello World application
+ * Only for studying purposes
  */
 package helloworld;
 
@@ -15,7 +14,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Simple hello world
         System.out.println("Hello All!");   
     }
     
